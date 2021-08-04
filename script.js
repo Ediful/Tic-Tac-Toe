37d2529d7315f0568617224c9bb96a774a57ea36
Ediful <56943939+Ediful@users.noreply.github.com>
2021-08-03 20:53:41 -0500

    converted to using classes

--- script.js
+++ script.js
@@ -1,108 +1,96 @@
-//  Module for gameboard
-const gameBoard = (() => {
-  let array = ["", "", "", "", "", "", "", "", ""];
+class Board {
+  array = ["", "", "", "", "", "", "", "", ""];
   
-  const get = () => array;
+  getBoard () {
+    return this.array;
+  }
 
-  const set = (index, xo) => {
-    array[index] = xo;
-  };
+  resetBoard () {
+    this.array = ["", "", "", "", "", "", "", "", ""];
+  }
 
-  const reset = () => {
-    array = ["", "", "", "", "", "", "", "", ""];
-  };
+  playerTurn (index, value) {
+    this.array[index] = value;
+  }
 
-  const gameOver = (turnSymbol) => {
-    if (winCheck(0,1,2,turnSymbol)) return true;
-    if (winCheck(3,4,5,turnSymbol)) return true;
-    if (winCheck(6,7,8,turnSymbol)) return true;
-    if (winCheck(0,3,6,turnSymbol)) return true;
-    if (winCheck(1,4,7,turnSymbol)) return true;
-    if (winCheck(2,5,8,turnSymbol)) return true;
-    if (winCheck(0,4,8,turnSymbol)) return true;
-    if (winCheck(2,4,6,turnSymbol)) return true;
+  winCheck (index0, index1, index2, turnSymbol) {
+    if (this.array[index0] === turnSymbol & this.array[index1] === turnSymbol & this.array[index2] === turnSymbol) return true;
     else return false;
   }
 
-  function winCheck(index0, index1, index2, turnSymbol) {
-    if (array[index0] === turnSymbol & array[index1] === turnSymbol & array[index2] === turnSymbol) return true;
+  gameOver (turnSymbol) {
+    if (this.winCheck(0,1,2,turnSymbol)) return true;
+    if (this.winCheck(3,4,5,turnSymbol)) return true;
+    if (this.winCheck(6,7,8,turnSymbol)) return true;
+    if (this.winCheck(0,3,6,turnSymbol)) return true;
+    if (this.winCheck(1,4,7,turnSymbol)) return true;
+    if (this.winCheck(2,5,8,turnSymbol)) return true;
+    if (this.winCheck(0,4,8,turnSymbol)) return true;
+    if (this.winCheck(2,4,6,turnSymbol)) return true;
     else return false;
   }
+}
 
-  return {get, set, reset, gameOver};
-})();
+// Game Controller Class
+class Game {
+  player1Turn = true;
+  turnSymbol;
 
-
-//  Module for game controller
-const gameController = (() => {
-  let player1Turn; // boolean to hold turn status
-  let turnSymbol; // string to hold "X" or "O"
-
-  // Grid cell buttons
-  const array = Array.from(document.getElementsByClassName("grid-item"));
-  array.forEach(element => element.addEventListener("click", () => {
-    turnSymbol = player1Turn ? "X" : "O";
-    gameBoard.set(element.value, turnSymbol);
-    displayController.displayTurn(element);
-    if(gameBoard.gameOver(turnSymbol)) displayController.gameOver(player1Turn);
-    player1Turn = !player1Turn;
-  }));
-  
-  // Restart Button
-  document.getElementById("reset-button").addEventListener("click", () => {
-    gameBoard.reset();
-    displayController.resetGameBoard();
-  });
-
-  // Instructions for each state
-  const startMenuState = () => document.getElementById("start-button").addEventListener("click", () => {
+  startGame () {
     let player1Text = document.getElementById("player1-text").value;
     let player2Text = document.getElementById("player2-text").value;
-
+    
     if (!player1Text) player1Text = "Player 1";
     if (!player2Text) player2Text = "Player 2";
-
+    
     let player1 = new Player(player1Text);
     let player2 = new Player(player2Text);
-
+    
     displayController.displayPlayerNames(player1.name, player2.name);
     displayController.showGameboard();
-    player1Turn = true;
-  });
+  }
 
-  startMenuState();
-})();
+  playerTurn = (element) => {
+    this.turnSymbol = this.player1Turn ? "X" : "O";
 
+    gameBoard.playerTurn(element.value, this.turnSymbol);
+    displayController.displayTurn(element);
 
-//  Module for display controller
-const displayController = (() => {
-  const displayTurn = (element) => {
-    let array = gameBoard.get();
+    if(gameBoard.gameOver(this.turnSymbol)) displayController.gameOver(this.player1Turn);
+    
+    this.player1Turn = !this.player1Turn;
+  }
+}
+
+// Display Controller Class
+class Display {
+  displayTurn (element) {
+    let array = gameBoard.getBoard();
     document.getElementById(element.id).innerText = array[element.value];
     document.getElementById(element.id).disabled = true;
-  };
-  
-  const showGameboard = () => {
+  }
+
+  showGameboard () {
     document.getElementById("startmenu-container").style.display = "none";
     document.getElementById("gameboard-container").style.display = "block";
-  };
+  }
 
-  const resetGameBoard = () => {
+  resetGameBoard () {
     Array.from(document.getElementsByClassName("grid-item")).forEach(element => {
       document.getElementById(element.id).innerText = "";
       document.getElementById(element.id).disabled = false;
     });
     document.getElementById("winner").textContent = "";
-  };
+  }
 
-  const displayPlayerNames = (player1Name, player2Name) => {
+  displayPlayerNames (player1Name, player2Name) {
     document.getElementById("player1-name").textContent = player1Name;
     document.getElementById("player2-name").textContent = player2Name;
-  };
+  }
 
-  const gameOver = (player1Turn) => {
+  gameOver (player1Turn) {
     let playerName = "";
-
+    
     Array.from(document.getElementsByClassName("grid-item")).forEach(element => {
       document.getElementById(element.id).disabled = true;
     });
@@ -110,23 +98,35 @@ const displayController = (() => {
     if (player1Turn) playerName = document.getElementById("player1-name").textContent;
     else playerName = document.getElementById("player2-name").textContent;
     document.getElementById("winner").textContent = playerName + " Wins!";
-  };
-
-  return {displayTurn, showGameboard, resetGameBoard, displayPlayerNames, gameOver};
-})();
-
+  }
+}
 
 // Player Class
 class Player {
   constructor(name) {
     this.name = name;
   }
-
+  
   get name() {
     return this._name; 
   }
-
+  
   set name(value) {
     this._name = value;
   }
 }
+
+let gameBoard = new Board();
+let displayController = new Display();
+let gameController = new Game();
+
+
+document.getElementById("start-button").addEventListener("click", gameController.startGame);
+
+document.getElementById("reset-button").addEventListener("click", () => {
+  gameBoard.resetBoard();
+  displayController.resetGameBoard();
+});
+
+const array = Array.from(document.getElementsByClassName("grid-item"));
+array.forEach(element => element.addEventListener("click", () => gameController.playerTurn(element)));
